08e31eb708216134b21151eb580bb996ed84c8a8
Arijana <102183538+ArijanaM@users.noreply.github.com>
2022-12-05 14:39:26 +0200

    added a map (row16-25)

--- javascript.js
+++ javascript.js
@@ -13,7 +13,16 @@ let icao_start;                       //ICAO of the depature airport
 const icao_rovaniemi = 'EFRO';               //ICAO of Rovaniemi
 let currentAirport;
 
+//map
+const map = L.map('map').setView([51.505, -0.09], 8);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
 
 
 
