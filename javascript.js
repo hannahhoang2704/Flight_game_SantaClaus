527f9344fbd2dc3b9cf28f008aaddb1792a371bb
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2022-12-06 10:56:52 +0200

    add map function and mark the airport in the map

--- javascript.js
+++ javascript.js
@@ -14,22 +14,42 @@ const icao_rovaniemi = 'EFRO';               //ICAO of Rovaniemi
 let currentAirport;
 
 
+//maps function
+
+let map;
+
+function drawMapWithMarker(pos, icao) {
+  // Use the leaflet.js library to show the location on the map (https://leafletjs.com/)
+  // first the map itself if it has not been created before
+  if (map == null) {
+    map = L.map('map')
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+  }
+  map.setView(pos, 13);
+
+  // then the marker
+  L.marker(pos).addTo(map).bindPopup(icao).openPopup();
+}
 
 
-
-
-//retrieve player's name in database
-function get_player_name(evt){
-  evt.preventDefault();
-  const name = document.querySelector('#user-name').value;
-  document.getElementById('welcome').innerText = `Hi ${playerName.value}, let's start your journey!`;
-  async function player_name(){
+//player's name
+async function player_name(){
     try{
-      const gameData = await fetch('http://127.0.0.1:3000/')
+      const gameData = await fetch('http://127.0.0.1:3000/gamerinfo?player=' + name)
+      const jsonName = await gameData.json();
+      console.log(jsonName)
     }catch(error){
       console.log(error);
     }
   }
+//retrieve player's name in database
+function get_player_name(evt){
+  evt.preventDefault();
+  const name = document.querySelector('#user-name').value;
+  document.getElementById('welcome').innerText = `Hi ${name}, let's start your journey!`;
+  player_name();
 }
 playerName.addEventListener('submit', get_player_name);
 
@@ -39,7 +59,7 @@ playerName.addEventListener('submit', get_player_name);
   document.getElementById('welcome').innerText = `Hi ${playerName.value}, let's start your journey!`;
 })*/
 
-//fetch the airport info where you want to flight to
+//fetch the airport info(ICAO, name, long, lat) where you want to flight to
 async function getAirportPosition(icao){
   try {
           const response = await fetch('http://127.0.0.1:3000/airport/' + icao);    // starting data download, fetch returns a promise which contains an object of type 'response'
@@ -51,7 +71,7 @@ async function getAirportPosition(icao){
           currentAirport = jsonData.Name;
           console.log('current airport:' + currentAirport)
           // and draw the map
-          //drawMapWithMarker(pos, icao)
+          drawMapWithMarker(pos, icao)
 
       } catch (error) {
           console.log(error.message);
@@ -73,9 +93,9 @@ async function getAirportDistance(icao_start, icao_end) {
     } catch (error) {
         console.log(error.message);
     } finally {                                         // finally = this is executed anyway, whether the execution was successful or not
-        return jsonData.dist
+        return jsonData.dist;
     }
-};
+}
 
 //promise 2 asynchronous
 async function run_process(){
@@ -92,7 +112,7 @@ async function run_process(){
 }
 
 //Check how far from departure airport to Rovaniemi
-function airport_start(evt){
+function airport_start(evt) {
   evt.preventDefault();
   const selectOption = document.getElementById('airport-fetch');          //get the ICAO number of the chosen airport
   icao_start = selectOption.options[selectOption.selectedIndex].value;
@@ -104,66 +124,67 @@ function airport_start(evt){
 }
 
 
-
-function airport_route() {
-  getAirportDistance(icao_start, icao_rovaniemi).then(function(distance) {
-   console.log('Distance = ' + distance + 'km')
-  })
-}
+//print in console the distance between departure airport to rovaniemi airport
+  function airport_route() {
+    getAirportDistance(icao_start, icao_rovaniemi).then(function(distance) {
+      console.log('Distance = ' + distance + 'km')
+    })
+  }
 
 
 
 //All the airports are listed after player choose the city
-function airportOptions(jsonData){
-  ICAO_p.innerText = 'List of airports:';                                 //Make a dropdrown list of depature airports
-  div_ICAO.appendChild(ICAO_p);
-  const select = document.createElement('select');
-  select.setAttribute('id', 'airport-fetch')
-  div_ICAO.appendChild(select)
-  for (let i = 0; i <jsonData.length; i++){
-    const airport_options = document.createElement('option');
-    airport_options.setAttribute('value', `${jsonData[i]['ICAO']}`);
-    airport_options.appendChild(document.createTextNode(`${jsonData[i]['Airport name']}`));
-    select.appendChild(airport_options);
+  function airportOptions(jsonData) {
+    ICAO_p.innerText = 'List of airports:';                                 //Make a dropdrown list of depature airports
+    div_ICAO.appendChild(ICAO_p);
+    const select = document.createElement('select');
+    select.setAttribute('id', 'airport-fetch')
+    div_ICAO.appendChild(select)
+    for (let i = 0; i < jsonData.length; i++) {
+      const airport_options = document.createElement('option');
+      airport_options.setAttribute('value', `${jsonData[i]['ICAO']}`);
+      airport_options.appendChild(document.createTextNode(`${jsonData[i]['Airport name']}`));
+      select.appendChild(airport_options);
+    }
+    const button_airport = document.createElement('button');      //Button to choose the departure airport
+    button_airport.appendChild(document.createTextNode('Take off!'));
+    button_airport.setAttribute('id', 'button-airport');
+
+    div_ICAO.appendChild(button_airport);
+
+////////////////post method to get ICAO of airport to flask server!!!!
+
+    button_airport.addEventListener('click', airport_start)           //click button to choose the departure airport
   }
-  const button_airport = document.createElement('button');      //Button to choose the departure airport
-  button_airport.appendChild(document.createTextNode('Take off!'));
-  button_airport.setAttribute('id', 'button-airport')
-  div_ICAO.appendChild(button_airport);
-
-////////////////post method to get ICAO of airport
-
-  button_airport.addEventListener('click', airport_start)           //click button to choose the departure airport
-}
-
 
 
 
 //fetch all the airports in the chosen city
-function fetch_airport(evt){
-  evt.preventDefault();
-  airportFetches();
-}
+  function fetch_airport(evt) {
+    evt.preventDefault();
+    airportFetches();
+  }
 
-async function airportFetches() {                 // asynchronous function is defined by the async keyword
-        div_ICAO.innerHTML = "";
-        console.log('asynchronous download begins');
-        try {                                               // error handling: try/catch/finally
-            console.log(city.value);
-            const response = await fetch('http://127.0.0.1:3000/' + city.value);    // starting data download, fetch returns a promise which contains an object of type 'response'
-            const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function
-            console.log(jsonData[0]['Airport name']);
+  async function airportFetches() {                 // asynchronous function is defined by the async keyword
+    div_ICAO.innerHTML = "";
+    console.log('asynchronous download begins');
+    try {                                               // error handling: try/catch/finally
+      console.log(city.value);
+      const response = await fetch('http://127.0.0.1:3000/' + city.value);    // starting data download, fetch returns a promise which contains an object of type 'response'
+      const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function
+      console.log(jsonData[0]['Airport name']);
 
-            airportOptions(jsonData);
-        } catch (error) {
-            console.log(error.message);
-            ICAO_p.innerText = `City can not found! Choose another city.`;        //inform the city is not in database
-            div_ICAO.appendChild(ICAO_p);
+      airportOptions(jsonData);
+    } catch (error) {
+      console.log(error.message);
+      ICAO_p.innerText = `City can not found! Choose another city.`;        //inform the city is not in database
+      div_ICAO.appendChild(ICAO_p);
 
-        } finally {                                         // finally = this is executed anyway, whether the execution was successful or not
-            console.log('asynchronous load complete');
-        }
+    } finally {                                         // finally = this is executed anyway, whether the execution was successful or not
+      console.log('asynchronous load complete');
     }
+  }
+
 
 //call-back function to fetch all airport in chosen city
 search.addEventListener('click', fetch_airport);
