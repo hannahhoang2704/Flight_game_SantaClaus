7e6759473ae64c5187e20849bbcf6447384f8356
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2022-12-08 16:52:45 +0200

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -25,8 +25,69 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+rovaniemi_deg = [122.3, 9.1]
 
+"""
+def get5Airport(currentlocation):
+    airports_list =[currentlocation]
+    degree_list = []
+    pick_airport
+    visited_airport
+    for i in range(5):
+        deg_lat = currentlocation[0]
+         # Latitude and longitude degree of departure airport
+        deg_long = currentlocation[1]
 
+        coordinator_scope_x = rovaniemi_deg[
+                                  0] - deg_lat  # calculate the coordinator(x,y) scope within depature airport and Rovaniemi airport
+        coordinator_scope_y = rovaniemi_deg[1] - deg_long
+
+        dist_between_each_scope_x = round(coordinator_scope_x / 3, 4)
+        dist_between_each_scope_y = round(coordinator_scope_y / 3, 4)
+
+        ##transit via 5 different airports to play game and collect gift
+
+        # transit via 5 different airports before arrive Rovaniemi
+        print(airports_list[i])
+        if dist_between_each_scope_x > dist_between_each_scope_y:
+            dist_between_each_scope = dist_between_each_scope_x
+        else:
+            dist_between_each_scope = dist_between_each_scope_y
+
+        point_x = degree_list[i][0] + dist_between_each_scope
+        point_y = degree_list[i][1] + dist_between_each_scope
+
+        point_x_minus = degree_list[i][0] - dist_between_each_scope
+        point_y_minus = degree_list[i][1] - dist_between_each_scope
+
+        pick_airport(point_x_minus, point_x, point_y_minus, point_y)
+
+        airport_name = \
+            pick_airport(point_x_minus, point_x, point_y_minus, point_y)[
+                0]  # call the transit airport
+        airport_spot_deg = \
+            pick_airport(point_x_minus, point_x, point_y_minus, point_y)[1]
+
+        new_transit = Airport(airport_name, airport_spot_deg[0],
+                              airport_spot_deg[1])
+
+        new_transit.airport_info()  # call our the airport where you are
+        new_transit.weather_check()  # weather info of the transit
+
+        visited_airport(airport_name,
+                              new_transit.deg)  # add the transit airport to the list
+        dist_between_each_a = distance_between_each_airport(i + 1)
+        print(
+            f"You've flew {dist_between_each_a} km")  # Distance between each airport
+        co2_each_transit = api_co2(dist_between_each_a)
+        print(
+            f"You've just consumed {co2_each_transit}")  # Print out the co2 consumption after moved to new transit
+        print(
+            f"Total consumption: {round(player1.co2_add(co2_each_transit), 2)}")  # Total amount of C02 consumption
+        print(
+            f"Still {new_transit.calc_distance_to_Rov():.2f} km away from Rovaniemi")  # Distance from certain transit airport to Rovaniemi
+
+"""
 def questions_answers():
 
     # Questionaires and answers
@@ -196,7 +257,7 @@ def calculate_distance_between_airports():
     end_airport = fetch_airport_names_by_icao_code(args.get("end"))
     print(start_airport)
     print(end_airport)
-    d = distance.distance((start_airport[2], start_airport[3]), (end_airport[2], end_airport[3])).km
+    d = round(distance.distance((start_airport[2], start_airport[3]), (end_airport[2], end_airport[3])).km, 2)
     print(d)
     response = {
         "start": (start_airport[2], start_airport[3]),
