561c9735096bdfdb5bf2068a0d0d4315bb2a49c7
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2022-12-12 17:19:44 +0200

    Add loop

--- javascript.js
+++ javascript.js
@@ -20,7 +20,7 @@ const windSpeed = document.getElementById('airport-wind');
 const weatherIcon = document.getElementById('weather-icon');
 
 let icao_start;                       //ICAO of the depature airport
-const icao_rovaniemi = 'EFRO';//ICAO of Rovaniemi
+const icao_rovaniemi = 'EFRO';        //ICAO of Rovaniemi
 const pos_Rov = [66.565, 25.83];
 let distanceTotal;
 let currentAirport = ' ';
@@ -29,15 +29,18 @@ let currentAirport_lat;
 let gifts;
 
 //main-game
+const mainProgramDiv = document.getElementById('main-program');
 const questionUpdate = document.getElementById('question');
-const mainGameDiv = document.getElementById('main-program');
 const answerSelection = document.getElementById('answer-selection');
 let correctAnswer;
 
+//Rock Paper Scicssors
+const rps_div = document.getElementById('rps');
+
 //update gifts
 //giftsUpdate.innerText = gifts;
-//Add maps
 
+//Add maps
 let map;
 
 
@@ -51,10 +54,10 @@ function drawMapWithMarker(pos, icao) {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
   }
-  map.setView(pos, 13);
+  map.setView(pos, 100);
 
   // then the marker
-  L.marker(pos).addTo(map).bindPopup('Starting Point').openPopup();
+  L.marker(pos).addTo(map).bindPopup(icao).openPopup();
 }
 
 function drawMapWithMarkerRovaniemi(pos_Rov, icao_rovaniemi) {
@@ -66,11 +69,12 @@ function drawMapWithMarkerRovaniemi(pos_Rov, icao_rovaniemi) {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
   }
-  map.setView(pos_Rov, 13);
+  map.setView(pos_Rov, 100);
 
   // then the marker
-  L.marker(pos_Rov).addTo(map).bindPopup('Rovaniemi').openPopup();
+  L.marker(pos_Rov).addTo(map).bindPopup("Santa's House").openPopup();
 }
+
 
 // function to update the game status
 function updateGameStatus(screen_name, gifts, currentco2) {
@@ -79,36 +83,35 @@ function updateGameStatus(screen_name, gifts, currentco2) {
   currentco2Update.innerText = currentco2;
 }
 
+
+
+
+
 //function to update the weather in HTML
 function updateWeather(temp, condition, wind, icon) {
   temperature.innerHTML = `<span>${temp}°C</span>`;
   weatherCondition.innerHTML = `<span>${condition}</span>`;
   windSpeed.innerHTML = `<span>${wind} m/s</span>`;
-  weatherIcon.src = 'https://openweathermap.org/img/wn/' + str(icon) +
-      '@2x.png';
+  weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+  console.log(weatherIcon.src)
 }
 
-//change the quiz in main program div
-function quiz(question_screen) {
-  questionUpdate.innerHTML = `Answer the question below: <br> ${question_screen}`;
 
-}
+
 
 //fetch the airport info(ICAO, name, long, lat) where you want to flight to
 async function getAirportPosition(icao) {
   try {
 
     const userName = document.querySelector('#user-name').value;
-    document.getElementById(
-        'welcome').innerText = `Hi ${userName}, let's start your journey!`;
+    //document.getElementById('welcome').innerText = `Hi ${userName}, let's start your journey!`;
     const response = await fetch(
         'http://127.0.0.1:5100/gamerinfo?name=' + userName + '&location=' +
         icao);    // starting data download, fetch returns a promise which contains an object of type 'response'
     const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function
     const pos = [jsonData.airport.Lat, jsonData.airport.Long];                                  // create position array for leaflet library
-    console.log(jsonData);     // log the result to the console
-    console.log(jsonData.airport.Name, jsonData.airport.Lat,
-        jsonData.airport.Long);
+    console.log(jsonData);                                                              // log the result to the console
+    //console.log(jsonData.airport.Name, jsonData.airport.Lat,jsonData.airport.Long);
 
     // Show the airport name to the screen
     currentAirport = jsonData.airport.Name;
@@ -137,6 +140,7 @@ async function getAirportPosition(icao) {
         currentAirport_long);    // starting data download, fetch returns a promise which contains an object of type 'response'
     const jsonDataWeather = await responseWeather.json();
     console.log(jsonDataWeather);               // JsonDataweather objects
+    console.log(jsonDataWeather.icon)
     updateWeather(jsonDataWeather.temperature, jsonDataWeather.description,
         jsonDataWeather.wind, jsonDataWeather.icon);      //update weather
 
@@ -175,23 +179,55 @@ async function getAirportDistance(icao_start, icao_end) {
 
 }
 
+/*
+//fetch countdown Christmas API
+async function addCountDown(){
+  try {
+    const response = await fetch('http://127.0.0.1:5100/countdown');    // starting data download, fetch returns a promise which contains an object of type 'response'
+    const countDownData = await response.json();          // retrieving the data retrieved from the response object using the json() function
+    console.log(countDownData);
+    const statement = countDownData.statement;
+    document.getElementById('count-down').innerText = statement;
+  } catch (error) {
+    console.log(error.message);
+  }
+}*/
+
+//type effect
+function type_effect(text, id){
+let i = 0;
+let txt = `Do you think: ${text}`;
+var speed = 70;
+typeWriter()
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById(`${id}`).innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+}
+
+//change the quiz in main program div
+function quiz(question_screen) {
+  questionUpdate.innerHTML = `Answer the question below: <br> ${question_screen}`;
+
+}
 //Fetch the quiz game
 async function fetch_question_quiz() {
   try {
     const response = await fetch('http://127.0.0.1:5100/quiz');    // starting data download, fetch returns a promise which contains an object of type 'response'
     const quizData = await response.json();          // retrieving the data retrieved from the response object using the json() function
     console.log(quizData);
-    console.log(quizData.question);     // log the result to the console
+    //console.log(quizData.question);                // log the result to the console
     const question_screen = quizData.question;
     correctAnswer = quizData.answer;
     answerSelection.innerHTML = '';
-    quiz(question_screen);
-
+    type_effect(question_screen, 'question')
+    //quiz(question_screen);
     return question_screen;
   } catch (error) {
     console.log(error.message);
-  } finally {                                         // finally = this is executed anyway, whether the execution was successful or not
-    //console.log("log get airport distance")
   }
 };
 
@@ -202,15 +238,28 @@ function mathForGifts(changeValue) {
   return gifts;
 }
 
+//fetch gifts amount back to database and server
+async function updateGiftsInDB(gifts) {
+  try {
+    const response = await fetch('http://127.0.0.1:5100/updategifts/' + gifts);    // starting data download, fetch returns a promise which contains an object of type 'response'
+    const giftData = await response.json();          // retrieving the data retrieved from the response object using the json() function
+    return giftData;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+
+
 //fetch the reward/robbery that affect gift change from Database
 async function changeGift(change) {
   try {
     const response = await fetch('http://127.0.0.1:5100/giftschange/' + change);    // starting data download, fetch returns a promise which contains an object of type 'response'
     const giftChangeData = await response.json();          // retrieving the data retrieved from the response object using the json() function
     console.log(giftChangeData);
+    await mathForGifts(giftChangeData);
     console.log(gifts);
-    mathForGifts(giftChangeData);
-
+    await updateGiftsInDB(gifts);
   } catch (error) {
     console.log(error.message);
   }
@@ -223,25 +272,17 @@ async function checkAnswer() {
   console.log(playerAnswer);
   console.log(gifts);
   if (playerAnswer == correctAnswer) {
-    console.log('Correct answer');
+    //console.log('Correct answer');
     answerSelection.innerHTML = `Congrats! Your answer is correct. We send you some treasure box. Check your gifts now!`;
     changeGift('add');
   } else {
-    console.log('Wrong answer');
+    //console.log('Wrong answer');
     answerSelection.innerHTML = `Oops! Your answer is wrong. Sadly your gifts also got robbed! `;
     changeGift('deduct');
   }
 
-  setTimeout(() => {
-    clearContent('user-info');
-    clearContent('question');
-    clearContent('answer-selection');
-    clearContent('next-btn')
-  },5000)
-
-
   const next_button = document.createElement('button');
-  next_button.innerHTML = 'Next';
+  next_button.innerHTML = 'Next challenge';
   next_button.setAttribute('class', 'game-btn');
   document.getElementById('next-btn').appendChild(next_button);
   next_button.addEventListener('click', rock_paper_scissors);
@@ -253,7 +294,6 @@ async function checkAnswer() {
 
 //push true,false options and button for quiz game
 function pushQuiz() {
-
   const selectForQuiz = document.createElement('select');
   selectForQuiz.setAttribute('id', 'answer-select');
   const optionTrue = document.createElement('option');
@@ -276,41 +316,103 @@ function pushQuiz() {
 
 }
 
-//Rock paper scissor
-  function rock_paper_scissors() {
-    const rock = document.createElement('button');
-    rock.innerHTML = 'Rock';
-    rock.value = '0';
-    rock.setAttribute('class','game-btn')
-    document.getElementById('rock_paper_scissors').appendChild(rock);
-    const paper = document.createElement('button');
-    paper.innerHTML = 'Paper';
-    paper.value = '1';
-    paper.setAttribute('class','game-btn')
-    document.getElementById('rock_paper_scissors').appendChild(paper);
-    const scissor = document.createElement('button');
-    scissor.innerHTML = 'Scissor';
-    scissor.value = '2';
-    scissor.setAttribute('class','game-btn')
-    document.getElementById('rock_paper_scissors').appendChild(scissor);
 
+//Rock paper scissor
+function rock_paper_scissors() {
+  rps_div.innerHTML = "<p>Let's play rock paper scissors:</p><br>";
+
+  const selectRps = document.createElement('select');
+  selectRps.setAttribute('id', 'select-rps');
+  rps_div.appendChild(selectRps)
+  const rock = document.createElement('option');
+  rock.innerText = 'Rock';
+  rock.value = '0';
+  selectRps.appendChild(rock);
+  const paper = document.createElement('option');
+  paper.innerHTML = 'Paper';
+  paper.value = '1';
+  selectRps.appendChild(paper)
+  const scissor = document.createElement('option');
+  scissor.innerHTML = 'Scissor';
+  scissor.value = '2';
+  selectRps.appendChild(scissor)
+  const rpsButton = document.createElement('button');
+  rpsButton.setAttribute('class', 'game-btn');
+  rpsButton.innerText = 'Submit';
+  rps_div.appendChild(rpsButton)
+  rpsButton.addEventListener('click', rpsExecute);
+
+}
+
+  //fetch the computer options in RPS game
+async function checkComputerOption(playerValue) {                 // asynchronous function is defined by the async keyword
+  try {                                               // error handling: try/catch/finally
+    const response = await fetch('http://127.0.0.1:5100/rpsgame/' + playerValue);    // starting data download, fetch returns a promise which contains an object of type 'response'
+    const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function
+    console.log(jsonData);
+    console.log(jsonData.computerChoice, jsonData.status, jsonData.result);
+    const fetchResultDiv = document.getElementById('fetch-result-div');
+    const p = document.createElement('p');
+    p.appendChild(document.createTextNode("Computer chooses: "));
+    fetchResultDiv.appendChild(p);
+    const compValue = parseInt(jsonData.compImg);
+    const imgPlayerOption = document.createElement('img');
+    imgPlayerOption.src = `image/game-image/rock-paper-scissors/${compValue}.png`;
+    fetchResultDiv.appendChild(imgPlayerOption);
+    const h2 = document.createElement('h2');
+    h2.setAttribute('id', 'rps-statement');
+    h2.appendChild(document.createTextNode(`${jsonData.result}. Check your gifts update!`));
+    mainProgramDiv.appendChild(h2);
+    if (jsonData.status == -1){
+      changeGift('deduct');
+    }else if(jsonData.status == 1){
+      changeGift('add');
+    }
+    return jsonData
+  } catch (error) {
+    console.log(error.message);
   }
+}
+
+function showRpsResult(){
+  const gameResultDiv = document.createElement('div');
+  rps_div.appendChild(gameResultDiv)
+}
+
+//when button is clicked, collect the player's option in RPS game and compare with Computer game
+async function rpsExecute(evt){
+   evt.preventDefault();
+   const fetchResultDiv = document.createElement('div');
+   fetchResultDiv.setAttribute('id', 'fetch-result-div')
+   rps_div.appendChild(fetchResultDiv);
+   fetchResultDiv.innerHTML = "";
+   const p = document.createElement('p');
+   p.appendChild(document.createTextNode("You've choose: "));
+   fetchResultDiv.appendChild(p);
+   const selectRps = document.getElementById('select-rps');
+   const playerValue = selectRps.options[selectRps.selectedIndex].value;
+   console.log(playerValue)
+   const imgPlayerOption = document.createElement('img');
+   imgPlayerOption.src = `image/game-image/rock-paper-scissors/${playerValue}.png`;
+   fetchResultDiv.appendChild(imgPlayerOption);
+   console.log(playerValue)
+   await checkComputerOption(playerValue);
+   await showRpsResult();
+};
+
 
 //Check how far from departure airport to Rovaniemi
 async function airport_start(evt) {
   evt.preventDefault();
   const selectOption = document.getElementById('airport-fetch');          //get the ICAO number of the chosen airport
   icao_start = selectOption.options[selectOption.selectedIndex].value;              //get value of selected option in <select> element!!!
-  //console.log(icao_start);
 
   await getAirportPosition(icao_start); //fetch the departure airport info
-
   await getAirportDistance(icao_start, icao_rovaniemi);
   await co2Consumed(distanceTotal);
+  //await addCountDown();
   await fetch_question_quiz();
-
   pushQuiz();
-
 }
 
 /* draw the map with two positions and a line between them */
@@ -343,11 +445,7 @@ function airportOptions(jsonData) {
   button_airport.setAttribute('class', 'game-btn');
   button_airport.appendChild(document.createTextNode('Take off!'));
   button_airport.setAttribute('id', 'button-airport');
-
   div_ICAO.appendChild(button_airport);
-
-////////////////post method to get ICAO of airport to flask server!!!!
-
   button_airport.addEventListener('click', airport_start); //click button to choose the departure airport
   button_airport.onclick = function() {
     document.getElementById('user-info').innerHTML = '';
@@ -373,9 +471,9 @@ async function airportFetches() {                 // asynchronous function is de
     ICAO_p.innerText = `City can not found! Choose another city.`;        //inform the city is not in database
     div_ICAO.appendChild(ICAO_p);
 
-  } finally {                                         // finally = this is executed anyway, whether the execution was successful or not
   }
 }
 
 //call-back function to fetch all airport in chosen city
 search.addEventListener('click', fetch_airport);
+
