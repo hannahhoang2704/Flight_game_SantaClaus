aba5844081da229a3ddf783d95cafe38d007acb2
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2022-12-12 17:00:00 +0200

    add loop

--- main_game.py
+++ main_game.py
@@ -1,20 +1,19 @@
-import mysql.connector                                  #fetch database
+import mysql.connector  # fetch database
 import random
 from flask import Flask, request, render_template
 from flask_cors import CORS
-from geopy import distance                              #calculate distance geopy
-#from game_project import Player
+from geopy import distance  # calculate distance geopy
+# from game_project import Player
 import requests
-import json                                             #Json structure
-from count_down import statement                        #Countdown API
-from questions_func import questions_answers            #quiz function
-from rock_paper_scissor import rock_paper_scissors      #rock-paper-scissors function
-
-
+import json  # Json structure
+from count_down import statement  # Countdown API
+from questions_func import questions_answers  # quiz function
+from rock_paper_scissor import rock_paper_scissors  # rock-paper-scissors function
+#import jsonpickle
 
 connection = mysql.connector.connect(
     host='127.0.0.1',
-    port= 3306,
+    port=3306,
     database='game_project',
     user='root',
     password='MyN3wP4ssw0rd',  # !QAZ2wsx#EDC or MyN3wP4ssw0rd ##CHECK PASSWORD TO RUN THE PROGRAM!!!
@@ -24,12 +23,80 @@ connection = mysql.connector.connect(
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+visited_airports = []
+
+rounds = 5
+
+#http://127.0.0.1:5000/next_airport/efhk
+@app.route('/next_airport/<current>')
+def next_airport(current):
+    global rounds
+
+    current_ICAO = str(current)
+    print(current_ICAO)
+    print('kala')
+    roi = 'EFRO'
+    degs1 = airport_position_by_icao(current_ICAO)
+    degs2 = airport_position_by_icao(roi)
+    deg1_lat = degs1[0]
+    deg1_long = degs1[1]
+    dist_to_rov = distance.distance(degs1, degs2).km
+    print(dist_to_rov)
+    rovaniemi_deg = degs2
+    coordinator_scope_x = rovaniemi_deg[
+                              0] - deg1_lat  # calculate the coordinator(x,y) scope within depature airport and Rovaniemi airport
+    coordinator_scope_y = rovaniemi_deg[1] - deg1_long
+
+    dist_between_each_scope = round(dist_to_rov / 3, 4)
+
+    x = deg1_lat + dist_between_each_scope
+    y = deg1_long + dist_between_each_scope
+
+    x1 = deg1_lat - dist_between_each_scope
+    y1 = deg1_long - dist_between_each_scope
+    list_of_icao = []
+    list_of_airport = []
+    list_of_deg = []
+    sql = "SELECT name, latitude_deg, longitude_deg, ident FROM airport"
+    sql += " WHERE latitude_deg between " + str(x1) + " and " + str(x) + \
+           " and longitude_deg between " + str(y1) + " and " + str(y)
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            list_of_airport.append(row[0])
+            list_of_deg.append([row[1], row[2]])
+            list_of_icao.append(row[3])
+            # print(row)
+            # print(list_of_airport)
+    random_nr = random.randint(0, len(list_of_airport) - 1)
+    while distance.distance(list_of_deg[random_nr],
+                            rovaniemi_deg).km > distance.distance(degs1,
+                                                                  rovaniemi_deg).km or distance.distance(
+        list_of_deg[random_nr],
+        rovaniemi_deg).km == 0 or \
+            distance.distance(list_of_deg[random_nr], rovaniemi_deg).km == distance.distance(degs1, rovaniemi_deg).km:
+        random_nr = random.randint(0, len(list_of_airport) - 1)
+    print(list_of_airport[random_nr], list_of_deg[random_nr], list_of_icao[random_nr])
+
+    rounds -= 1
+
+    response = {
+        "ICAO": list_of_icao[random_nr],
+        "Round": rounds,
+        "Name": list_of_airport[random_nr],
+        "Degrees": list_of_deg[random_nr],
+        "DistanceToRoi": distance.distance(list_of_deg[random_nr], rovaniemi_deg).km,
+        "DistanceFromLast": distance.distance(degs1,degs2).km
+
+    }
+    return response
 
 
-
-#Quiz game function
+# Quiz game function
 def questions_answers():
-
     # Questionaires and answers
     questions = (
         "Wasting less food is a way to reduce greenhouse gas emissions.",  # 0
@@ -71,7 +138,6 @@ def questions_answers():
                "true",  # 14
                "false")  # 15
 
-
     questions = list(questions)
     answers = list(answers)
     random_index_number = random.randint(0, len(questions) - 1)
@@ -80,8 +146,7 @@ def questions_answers():
     return random_ques, right_answer
 
 
-
-#Rock-paper-scissors game
+# Rock-paper-scissors game
 @app.route('/rpsgame/<value>')
 def rock_paper_scissors(value):
     player_score = 0
@@ -97,7 +162,7 @@ def rock_paper_scissors(value):
                      ["You win", "It\'s a draw", "You lose"],
                      ["You lose", "You win", "It\'s a draw"]]
     status_matrix = [[0, -1, 1],
-                     [1, 0, -1],            #0: it's draw, -1: you lost, 1: you won
+                     [1, 0, -1],  # 0: it's draw, -1: you lost, 1: you won
                      [-1, 1, 0]]
     result = choice_matrix[user_choice][computer_choice]
     status = status_matrix[user_choice][computer_choice]
@@ -114,20 +179,7 @@ def rock_paper_scissors(value):
     return response
 
 
-def airport_position_by_icao(ICAO):
-    sql = "SELECT name, latitude_deg, longitude_deg from airport"
-    sql += " WHERE ident='" + ICAO + "'"
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    if cursor.rowcount > 0:
-        for row in result:
-            deg = [row[1], row[2]]
-            # print(deg)
-    return deg
-
-
-#Check the quiz game
+# Check the quiz game
 @app.route('/quiz')
 def quiz_game():
     question, answer = questions_answers()
@@ -138,7 +190,8 @@ def quiz_game():
     print(question, answer)
     return response
 
-#change point of the game
+
+# change point of the game
 
 def get_gift():
     nr_random = random.randint(1, 5)
@@ -153,6 +206,7 @@ def get_gift():
             giftplus = int(row[1])
             print(f"You got {row[0]}, and earn {row[1]} gifts")
     return giftplus
+
 
 def deduct_gift():
     nr_random = random.randint(6, 8)
@@ -169,7 +223,7 @@ def deduct_gift():
     return giftminus
 
 
-#make change in gifts
+# make change in gifts
 @app.route('/giftschange/<change>')
 def change_in_gift(change):
     if change == 'add':
@@ -178,10 +232,11 @@ def change_in_gift(change):
         gifts_change = str(deduct_gift())
     return gifts_change
 
-#update gifts in DB
+
+# update gifts in DB
 @app.route('/updategifts/<gifts>')
 def update_gifts_in_DB(gifts):
-    #gifts = float(gifts)
+    # gifts = float(gifts)
     sql = "UPDATE game"
     sql += " SET gifts=" + gifts + " ORDER BY ID DESC LIMIT 1"
     print(sql)
@@ -191,7 +246,7 @@ def update_gifts_in_DB(gifts):
     return gifts
 
 
-#gamer input departure airport and name
+# gamer input departure airport and name
 @app.route('/gamerinfo')
 def gamerinfo():
     args = request.args
@@ -199,15 +254,15 @@ def gamerinfo():
     location = args.get('location')
     player_name = name
     departure_ICAO = location
-    #print(player_name, departure_ICAO)
+    # print(player_name, departure_ICAO)
     sql = "INSERT into game (screen_name, location) "
     sql += "VALUES ('" + name + "','" + location + "')"
-    #print(sql)
+    # print(sql)
     cursor = connection.cursor(dictionary=True)
     cursor.execute(sql)
     sql2 = "SELECT * from game "
-    sql2 += "WHERE id=" + str(cursor.lastrowid) +";"
-    #print(sql2)
+    sql2 += "WHERE id=" + str(cursor.lastrowid) + ";"
+    # print(sql2)
     cursor.execute(sql2)
     result = cursor.fetchone()
     ap = airport(location)
@@ -215,8 +270,9 @@ def gamerinfo():
     print(json.dumps(result))
     return result
 
-#Countdown to Christmas API
-#http://127.0.0.1:5100/countdown
+
+# Countdown to Christmas API
+# http://127.0.0.1:5100/countdown
 @app.route('/countdown')
 def countdown():
     request = "https://christmas-days.anvil.app/_/api/get_days"
@@ -229,9 +285,10 @@ def countdown():
         'statement': statement}
     return result
 
-#fetch airport in the city
+
+# fetch airport in the city
 def municipality_search(city):
-    #icao_suggested_list = []
+    # icao_suggested_list = []
     sql = "SELECT ident, name FROM airport"
     sql += " WHERE municipality='" + city + "'"
     # icao_list = []
@@ -242,33 +299,48 @@ def municipality_search(city):
     if cursor.rowcount > 0:
         for row in result:
             print(f"      {row[0]}: {row[1]}")
-            airport_info= {}
+            airport_info = {}
             airport_info['ICAO'] = row[0]
             airport_info['Airport name'] = row[1]
             airport_list.append(airport_info)
         return airport_list
-    #print(icao_suggested_list)
+    # print(icao_suggested_list)
     return airport_list
 
 
-#Fetch all airports in chosen city
+# Fetch all airports in chosen city
 @app.route('/<city>')
 def airport_search(city):
     response = municipality_search(city)
     return response
 
 
+def airport_position_by_icao(ICAO):
+    sql = "SELECT name, latitude_deg, longitude_deg from airport"
+    sql += " WHERE ident='" + ICAO + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    deg = []
+    if cursor.rowcount > 0:
+        for row in result:
+            deg = [row[1], row[2]]
+            print(deg)
+        return deg
+
+
 def fetch_airport_names_by_icao_code(icao):
-    sql  = "SELECT ID, ident, name, municipality, latitude_deg, longitude_deg FROM airport"
+    sql = "SELECT ID, ident, name, municipality, latitude_deg, longitude_deg FROM airport"
     sql += " WHERE ident='" + icao + "'"
     db_cursor = connection.cursor()
     db_cursor.execute(sql)
     query_result = db_cursor.fetchall()
     if db_cursor.rowcount > 0:
-        for row in query_result:        # get only the first match
+        for row in query_result:  # get only the first match
             return row[2], row[3], row[4], row[5]
 
     return "", "", "", ""
+
 
 @app.route('/airport/<icao>')
 def airport(icao):
@@ -283,17 +355,17 @@ def airport(icao):
     return response
 
 
-#Fetch weather condition when entering longitude and latitude of the airport
+# Fetch weather condition when entering longitude and latitude of the airport
 
 @app.route('/weather')
 def weather():
     args = request.args
     latitude = args.get('lat')
     longitude = args.get('long')
-    #print(longitude, latitude)
+    # print(longitude, latitude)
     request_link = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=aa8e935a3667b6a53c9bf49d4ba2904a"
     response = requests.get(request_link).json()
-    #print(json.dumps(response, indent=2))
+    # print(json.dumps(response, indent=2))
     description = response["weather"][0]["description"]
     temp = response["main"]["temp"]
     wind = response['wind']['speed']
@@ -324,7 +396,8 @@ def calculate_distance_between_airports():
     }
     return response
 
-#API to calculate C02 consumption
+
+# API to calculate C02 consumption
 
 @app.route('/co2consumed/<d>')
 def api_co2(d):
@@ -335,7 +408,7 @@ def api_co2(d):
     print(response)
     amount = str(response[0]['co2'])
     sql = "UPDATE game"
-    sql += " SET co2_consumed=" + amount +" ORDER BY ID DESC LIMIT 1"
+    sql += " SET co2_consumed=" + amount + " ORDER BY ID DESC LIMIT 1"
     print(sql)
     cursor = connection.cursor()
     cursor.execute(sql)
@@ -343,7 +416,5 @@ def api_co2(d):
     return amount
 
 
-
-
 if __name__ == '__main__':
-    app.run(use_reloader=True, host='127.0.0.1', port=5100)
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
